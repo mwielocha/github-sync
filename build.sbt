7986148bf80fc46d1876e2686986118af849dec6
mwielocha <mwielocha@icloud.com>
2020-06-19 20:12:20 +0200

    Boot with assembly

--- build.sbt
+++ build.sbt
@@ -11,23 +11,40 @@ lazy val root = (project in file(".")).
 mainClass in (Compile, run) := Some("io.mwielocha.githubsync.Boot")
 
 val akkaVersion = "2.6.4"
+val slickVersion = "3.3.2"
 val circeVersion = "0.13.0"
 val logbackVersion = "1.2.3"
 val factorioVersion = "0.2.0"
 val akkaHttpVersion = "10.1.12"
 val shapelessVersion = "2.3.3"
 val scalatestVersion = "3.1.1"
+val pureConfigVersion = "0.12.3"
 val scalaLoggingVersion = "3.9.2"
 val akkaHttpCirceVersion = "1.31.0"
+
+scalacOptions ++= Seq(
+  "-language:postfixOps",
+  "-language:implicitConversions",
+  "-deprecation",
+  "-feature",
+  "-Yrangepos",
+  "-language:existentials",
+  "-language:higherKinds",
+  "-language:postfixOps",
+  "-Xmacro-settings:factorio-verbose"
+)
 
 libraryDependencies ++= Seq(
   "io.circe" %% "circe-core" % circeVersion,
   "io.circe" %% "circe-generic" % circeVersion,
   "io.circe" %% "circe-generic-extras" % circeVersion,
+  "com.typesafe.slick" %% "slick" % slickVersion,
+  "com.typesafe.slick" %% "slick-hikaricp" % slickVersion,
   "com.chuusai" %% "shapeless" % shapelessVersion,
   "io.mwielocha" %% "factorio-core" % factorioVersion,
   "io.mwielocha" %% "factorio-annotations" % factorioVersion,
   "io.mwielocha" %% "factorio-macro" % factorioVersion % "provided",
+  "com.github.pureconfig" %% "pureconfig" % pureConfigVersion,
   "com.typesafe.akka" %% "akka-stream" % akkaVersion,
   "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
   "de.heikoseeberger" %% "akka-http-circe" % akkaHttpCirceVersion,
--- src/main/scala/io/mwielocha/githubsync/Boot.scala
+++ src/main/scala/io/mwielocha/githubsync/Boot.scala
@@ -1,5 +1,54 @@
 package io.mwielocha.githubsync
 
-object Boot extends App {
-  println("hi")
+import akka.Done
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.ToResponseMarshaller
+import akka.http.scaladsl.model.{ HttpRequest, HttpResponse, StatusCode }
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import com.typesafe.scalalogging.LazyLogging
+import factorio._
+import com.typesafe.config.ConfigFactory
+import io.mwielocha.githubsync.blueprints.AppBlueprint
+import factorio.annotations.blueprint
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import pureconfig._
+import pureconfig.generic.auto._
+import io.mwielocha.githubsync.config.AppConfig
+import pureconfig.error.ConfigReaderFailures
+
+object Boot extends App with LazyLogging {
+
+  logger.info("Starting up...")
+
+  ConfigSource.default.load[AppConfig] match {
+    case Right(config) => boot(config)
+    case Left(ConfigReaderFailures(head, _)) =>
+      logger.error("Missconfigured: {}", head.description)
+  }
+
+  def boot(config: AppConfig) = {
+
+    val blueprint = new AppBlueprint(config)
+
+    val assemble = Assembler[Application](blueprint)
+
+    val application = assemble()
+
+    import application.actorSystem
+
+    val http = Http()
+
+    http.bindAndHandle(application.routes(), config.host, config.port)
+
+    sys.addShutdownHook {
+      Await.result(
+        application
+          .actorSystem
+          .terminate(),
+        1 second)
+    }
+  }
 }
